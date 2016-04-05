50a4d0fba9024c893d91acf3c74051ab430583ec
giovani <giovanicardona@saludfamiliar>
2016-04-05 18:55:55 -0500

    - Se crea activity para registro de canchas - Se crea directorio /files para documentos de proyecto

--- app/src/main/java/com/vel/dar/her/tododeportes/MainActivity.java
+++ app/src/main/java/com/vel/dar/her/tododeportes/MainActivity.java
@@ -1,13 +1,12 @@
 package com.vel.dar.her.tododeportes;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -86,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void seleccionSeccion(View v) {
+        startActivity(new Intent(this, RegistrarCanchaActivity.class));
         switch (v.getId()) {
             case R.id.seccionCanchas:
                 Toast.makeText(getApplicationContext(), "Canchas", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/vel/dar/her/tododeportes/RegistrarCanchaActivity.java
@@ -0,0 +1,58 @@
+package com.vel.dar.her.tododeportes;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class RegistrarCanchaActivity extends ActionBarActivity {
+    private Spinner spnTipo;
+    private Spinner spnSubtipo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_cancha);
+
+        spnTipo = (Spinner) findViewById(R.id.spnTipo);
+        spnSubtipo = (Spinner) findViewById(R.id.spnSubtipo);
+
+        // TIPO
+        ArrayAdapter<CharSequence> adapterTipos = ArrayAdapter.createFromResource(this,
+                R.array.arrTipos, android.R.layout.simple_spinner_item);
+        adapterTipos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnTipo.setAdapter(adapterTipos);
+
+        // SUBTIPO
+        ArrayAdapter<CharSequence> adapterSubtipos = ArrayAdapter.createFromResource(this,
+                R.array.arrSubtipos, android.R.layout.simple_spinner_item);
+        adapterSubtipos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSubtipo.setAdapter(adapterSubtipos);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registrar_cancha, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
