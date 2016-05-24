90ede6749b5d61425d20f42cff60410084bf1bed
Hernan Velasquez <hernanudea@gmail.com>
2016-05-23 22:28:00 -0500

    Se crea rama "desarrollo" y se crea activity para guardar usuarios

---
+++ app/src/main/java/com/tododeportes/tododeportesapp/gui/RegistrarUsuarioActivity.java
@@ -0,0 +1,15 @@
+package com.tododeportes.tododeportesapp.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tododeportes.tododeportesapp.R;
+
+public class RegistrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+}
